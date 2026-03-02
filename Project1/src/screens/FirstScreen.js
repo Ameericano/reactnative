4c8fdc78ac0fc73df1296e5e19e1753f19644006
Ameericano <ameerakmal2020@gmail.com>
2026-03-02 20:55:04 +0800

    alert-feature

--- Project1/src/screens/FirstScreen.js
+++ Project1/src/screens/FirstScreen.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, Alert } from 'react-native';
 
 const FirstScreen = () => {
+  
+  const openAlert = () => {
+    alert("You Clicked the Button!");
+  };
+
   return (
-    <View>
-      <Text>Hello!</Text>
-      <Button title = "This is a button"/>
+
+    <View style={{
+      flex:1,
+      alignItems:'center',
+      justifyContent:'center'
+      }}>
+
+      <Text style={{margin:16}}>
+
+        Hello 
+
+        <Text style = {{color:'red'}}> World</Text> !
+        
+      </Text>
+      <Button
+       title = "This is a button"
+       onPress={openAlert}
+       />
+
     </View>
   );
 };
+
 
 export default FirstScreen;
