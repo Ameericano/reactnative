e26f38d5c2095590184f6111f1bf6d0cdcccd2a1
Ameericano <ameerakmal2020@gmail.com>
2026-03-09 20:18:50 +0800

    Third Screen

--- Project1/App.tsx
+++ Project1/App.tsx
@@ -1,11 +1,12 @@
 import { SafeAreaProvider } from "react-native-safe-area-context"
 import FirstScreen from "./src/screens/FirstScreen"
 import SecondScreen from "./src/screens/SecondScreen"
+import ThirdScreen from "./src/screens/ThirdScreen"
 
 const App = () => {
   return (
     <SafeAreaProvider>
-      <SecondScreen></SecondScreen>
+      <ThirdScreen></ThirdScreen>
     </SafeAreaProvider>
   )
 }
---
+++ Project1/src/screens/ThirdScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+const ThirdScreen = () => {
+    return(
+        <View>
+            <Text>
+                Pikachu
+            </Text>
+        </View>
+    )
+}
+
+export default ThirdScreen
