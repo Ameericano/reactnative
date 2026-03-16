40421f085f822363b2a22cad83e2c6a057638906
Ameericano <ameerakmal2020@gmail.com>
2026-03-16 20:23:46 +0800

    Fourth Screeen

--- Project1/App.tsx
+++ Project1/App.tsx
@@ -3,11 +3,12 @@ import FirstScreen from "./src/screens/FirstScreen"
 import SecondScreen from "./src/screens/SecondScreen"
 import AddImage from "./src/screens/ThirdScreen"
 import ImageScreen from "./src/screens/ImageScreen"
+import FourthScreen from "./src/screens/FourthScreen"
 
 const App = () => {
   return (
     <SafeAreaView style = {{flex: 1}}>
-      <ImageScreen></ImageScreen>
+      <FourthScreen></FourthScreen>
     </SafeAreaView>
   )
 }
