925e6970934dbc475faa1bbcb132af2a61fba35d
Ameericano <ameerakmal2020@gmail.com>
2026-05-18 21:26:15 +0800

    Project Submission

---
+++ SolarSystem/src/components/PlanetComponents.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+
+export const PlanetComponents = props => {
+  const { image, name, desc, alerttext } = props;
+  const openAlert = () => {
+    alert(alerttext)
+  }
+  return (
+    <View style={{ flex: 1 }}>
+      <TouchableOpacity onPress={() => openAlert()}>
+        <View
+          style={{
+            flexDirection: 'row',
+            backgroundColor: 'wheat',
+            padding: 16,
+            margin: 16,
+            borderRadius: 20,
+            borderColor: 'green',
+            borderWidth: 1,
+          }}
+        >
+          <Image
+            source={image}
+            style={{
+              width: 100,
+              height: 100,
+            }}
+          />
+          <View
+            style={{
+              flex: 1,
+              marginLeft: 8,
+              marginRight: 8,
+              justifyContent: 'center',
+            }}
+          >
+            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>{name}</Text>
+            <Text style={{ fontSize: 15 }}>{desc}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
---
+++ SolarSystem/src/screens/SolarSystemScreen.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Image, ScrollView, Text, View } from 'react-native';
+import {PlanetComponents} from "../components/PlanetComponents"
+
+const SolarSystemScreen = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <View
+        style={{
+          alignItems: 'center',
+          padding: 16,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 30,
+            fontFamily: 'serif',
+            textDecorationLine: 'underline',
+          }}
+        >
+          Solar System
+        </Text>
+      </View>
+      <ScrollView>
+        <PlanetComponents
+            name = "Mercury"
+            desc = "Mercury is the fastest planet, zipping around the sun every 88 earth day"
+            image = {require("../../assets/images/mercury.png")}
+            alerttext = "You opened mercury!"
+        />
+        <PlanetComponents
+            name = "Venus"
+            desc = "Venus spins slowly in the opposite direction from most planets"
+            image = {require("../../assets/images/venus.png")}
+             alerttext = "You opened venus!"
+        />
+        <PlanetComponents
+            name = "Earth"
+            desc = "Earth is the only place we know of so far that's inhabited by living thing"
+            image = {require("../../assets/images/earth.png")}
+             alerttext = "You opened earth!"
+        />
+        <PlanetComponents
+            name = "Mars"
+            desc = "Mars is a dusty, cold, desert world with a very thin atmosphere"
+            image = {require("../../assets/images/mars.png")}
+             alerttext = "You opened mars!"
+        />
+        <PlanetComponents
+            name = "Jupiter"
+            desc = "Jupiter is more than twice as massive than the other planets of our solar system combined"
+            image = {require("../../assets/images/jupiter.png")}
+             alerttext = "You opened jupiter!"
+        />
+        <PlanetComponents
+            name = "Saturn"
+            desc = "Adorned with a dazzling, complex system of icy rings, Saturn is unique in our solar system"
+            image = {require("../../assets/images/saturn.png")}
+             alerttext = "You opened saturn!"
+        />
+        <PlanetComponents
+            name = "Uranus"
+            desc = "Uranus rotates at a nearly 90-degree angle from the planet of its orbit"
+            image = {require("../../assets/images/uranus.png")}
+             alerttext = "You opened uranus!"
+        />
+        <PlanetComponents
+            name = "Neptune"
+            desc = "Neptune is dark, cold, and whipped by supersonic winds"
+            image = {require("../../assets/images/neptune.png")}
+             alerttext = "You opened neptune!"
+        />
+        
+      </ScrollView>
+    </View>
+  );
+};
+
+export default SolarSystemScreen;
